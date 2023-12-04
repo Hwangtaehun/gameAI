0c65dd57fe64db5a4e7c4c12a0b5aabb94a92df6
Hwangtaehun <r283@naver.com>
2023-12-04 22:49:48 +0900

    .....

--- finalexam/behaviortree/Assets/Script/AgentBehaviour.cs
+++ finalexam/behaviortree/Assets/Script/AgentBehaviour.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class AgentBehaviour : MonoBehaviour
 {
     public GameObject target;
+    public float maxAccel;
+    public float maxRotation;
+    public float maxAngularAccel;
     protected Agent agent;
 
     public virtual void Awake()
@@ -18,5 +21,19 @@ public class AgentBehaviour : MonoBehaviour
     public virtual Steering GetSteering()
     {
         return new Steering();
+    }
+
+    public float MapToRange(float rotation)
+    {
+        rotation %= 360.0f;
+        if (Mathf.Abs(rotation) > 180.0f)
+        {
+            if(rotation < 0.0f){
+                rotation += 360.0f;
+            }else{
+                rotation -= 360.0f;
+            }
+        }
+        return rotation;
     }
 }
